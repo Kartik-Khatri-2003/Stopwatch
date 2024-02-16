2e4a6f4c08d4209932a23585ff6e9278311f0574
Kartik <kartikkhatri2003@gmail.com>
2024-02-16 12:37:26 +0530

    add comments

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const pauseButton = document.querySelector("#pause");
 const playButton = document.querySelector("#play");
 const restartButton = document.querySelector("#restart");
 const displayTime = document.querySelector("#display");
+
 let timer = null;
+//This indicates that no interval timer is active when the page loads,
+//as the stopwatch hasn't started yet.
 
 let [seconds, minutes, hours] = [0,0,0];
 
@@ -25,10 +28,13 @@ function stopwatch(){
     displayTime.innerHTML = h+":"+m+":"+s;                       
 }
 
-function watchStart(){
+function watchStart() {
+    //Before starting a new timer, any existing timer (if present) is cleared using clearInterval(timer).
+    //This ensures that only one timer is running at a time
     if (timer!= null) {
         clearInterval(timer);
     }
+    // Start the stopwatch timer
     timer = setInterval(stopwatch,1000);
     playButton.disabled = true;
     playButton.classList.add("stylebtn");
@@ -58,3 +64,29 @@ function watchReset(){
 /* buttons.classList.remove("stylebtn"); */
 /* The above code is incorrect cause 'buttons' is a nodelist. */
 /* We need to iterate over each button of the NodeList using forEach() method. */
+
+/* New Topics : */
+
+/* setInterval-------
+   The setInterval function is used to repeatedly execute a specified function
+   or piece of code at a fixed time interval.
+   syntax: setINterval(function, interval);
+   1. function: The function to be executed at each interval.
+   2. interval: The time interval, in milliseconds, at which the function should be executed
+   For example: setInterval(stopwatch, 1000) would execute the stopwatch function every 1000 milliseconds (1 second).
+*/
+
+/* clearInterval-------
+   The clearInterval function is used to stop the repeated execution of code that was previously set up with setInterval.
+   syntax: clearInterval(intervalID); 
+   1. intervalID: The ID value returned by the setInterval function when it was originally called.
+   fFor example: clearInterval(timer); here the intervalID is 'timer'.
+*/
+
+
+
+
+
+
+
+
